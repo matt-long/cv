e792d00f7605ba8f135f03e53ec5c354c69dabcd
matt-long <mclong@ucar.edu>
2022-08-29 11:37:23 -0600

    new 2022 pubs

--- extract_pubs.py
+++ extract_pubs.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
 from subprocess import check_call
 
+bibfile = "publications-2021.bib"
 
-with open('publications.bib') as fid:
+with open(bibfile) as fid:
     lines = fid.readlines()
 
-keys = list(filter(lambda s: s[0] == '@', lines))
-keys = [s.strip().replace(',', '').split('{')[1] for s in keys]
+keys = list(filter(lambda s: s[0] == "@", lines))
+keys = [s.strip().replace(",", "").split("{")[1] for s in keys]
 
 preamble = [
-    r'\documentclass[11pt]{article}',
-    r'\begin{document}',
+    r"\documentclass[11pt]{article}",
+    r"\begin{document}",
 ]
 
 post = [
-    r'\bibliographystyle{agu-based-rev-num}',
-    r'\bibliography{publications}',
-    r'\end{document}',
+    r"\bibliographystyle{agu-based-rev-num}",
+    r"\bibliography{publications}",
+    r"\end{document}",
 ]
 
-with open('pubs.tex', 'w') as fid:
+with open("pubs.tex", "w") as fid:
     for line in preamble:
-        fid.write(f'{line}\n')
+        fid.write(f"{line}\n")
     for key in keys:
-        fid.write(f'\\nocite{{{key}}}\n')
+        fid.write(f"\\nocite{{{key}}}\n")
     for line in post:
-        fid.write(f'{line}\n')
+        fid.write(f"{line}\n")
+
+docxfile = bibfile.replace(".bib", ".docx")
 
 check_call(
-    ['pandoc', '-s', '--bibliography', 'publications.bib',
-    '--filter', 'pandoc-citeproc', 'pubs.tex', '-o', 'pubs.docx'
+    [
+        "pandoc",
+        "-s",
+        "--bibliography",
+        bibfile,
+        "--citeproc",
+        "pubs.tex",
+        "-o",
+        docxfile,
     ]
 )
+check_call(["rm", "-f", "pubs.tex"])
